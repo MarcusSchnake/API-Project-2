eef4681e2ec8cef5c4280c232d605ef58408e252
Marcus Schnake <marcusschnake@Marcuss-MBP.lan>
2021-10-07 17:44:26 -0400

    API pull and display

---
+++ public/index.js
@@ -0,0 +1,49 @@
+const baseURL = 'https://api.nasa.gov/techtransfer/software/?engine&api_key=bg9H6OM1WqeJbDCkkpHuHZdZ8LxXQ4oksHX79ha1'
+
+
+const prototypeName = document.getElementById('prototypeName');
+const softwareType = document.getElementById('softwareType')
+const button = document.getElementById('button');
+const gitRepository = document.getElementById('gitRepository')
+button.addEventListener('click', fetchNasaData)
+
+//fetch
+
+async function fetchNasaData() {
+    const response = await fetch(`${baseURL}`);
+    console.log(response);
+    const json = await response.json();
+    console.log(json)
+    displayData(json)
+
+
+}
+//display
+const displayData = (data) => {
+    const randResult = Math.floor(Math.random() * (38 - 2) + 2);
+    const mapResult = data.results[randResult].map((item)=> item)
+    console.log(mapResult, 'someString');
+    prototypeName.innerText = (data.results[randResult][5]);
+    softwareType.innerText = (data.results[randResult][2]);
+    softwareDescription.innerText = (data.results[randResult][3]);
+    gitRepository.querySelector.innerText = (data.results[randResult][8]);
+
+}
+
+// if (gitRepository == gitRepository[randResult][8]) {
+
+//     gitRepository = [randResult][8];
+
+// } else {
+//     (gitRepository == null)
+
+//     gitRepository = "No Public Repository";
+// }
+
+// console.log(gitRepository)
+
+
+
+
+fetchNasaData()
+
